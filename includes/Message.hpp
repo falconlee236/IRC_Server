a2a3dcebd62e0587e02fdbf898d1166ad9b7a000
falconlee236 <falconlee236@cau.ac.kr>
2024-07-16 18:40:26 +0900

    add command enum type in message class

--- includes/Message.hpp
+++ includes/Message.hpp
@@ -3,13 +3,35 @@
 
 #include <iostream>
 #include <string>
-#include <queue>
 #include <sstream>
+#include <vector>
 
 class Message {
+public:
+	typedef enum CMD {
+		NONE,  // 0
+		CAP,
+		PASS,
+		NICK,
+		USER,
+		QUIT,
+		JOIN,
+		PART,
+		TOPIC,
+		MODE,
+		INVITE,
+		KICK,
+		PRIVMSG,
+		PING,
+		CMD_SIZE
+	} e_cmd;
+
 private:
-	std::queue<std::string> msg_queue;
+	static const std::string _commandList[CMD_SIZE + 1];
+	e_cmd _cmd_type;
+	std::string _params;
 	std::vector<std::string> splitN(const std::string& str, char delimiter, size_t n);
+	e_cmd findCommand(const std::string cmd_str);
 
 public:
 	Message(void);
@@ -18,7 +40,8 @@ public:
 	Message(const Message& obj);
 	Message& operator= (const Message& obj);
 
-	void getClientMsg(std::string str);
+	e_cmd getCommand(void);
+	const std::string getParams(void);
 };
 
 #endif
