3a5b9109bf2299daaa966612db6df8a8c598259b
falconlee236 <falconlee236@cau.ac.kr>
2024-07-16 16:00:56 +0900

    add <<. >> operator in client class

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -1,7 +1,11 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
+#define BUFFER_SIZE 1024
+#define SOCKET_LINE_SIZE 512
+#define CRLF "\r\n"
 
 #include <unistd.h>
+#include <sys/socket.h>
 #include <string>
 #include <iostream>
 
@@ -10,11 +14,34 @@ private:
     const int _client_fd;
     const int _client_port;
     const std::string _client_ip;
+    std::string _read_data;
+    std::string _write_data;
 
 public:
     Client(void);
     Client(int client_fd, int client_port, std::string client_ip);
     ~Client();
+
+    Client &operator<<(const std::string &data);
+    Client &operator>>(std::string &data);
+
+    Client &operator<<(const int socket_fd);
+    Client &operator>>(const int socket_fd);
+    
+    class ClientDisconnectException : public std::exception{
+    public:
+        const char *what(void) const throw();
+    };
+
+    class CannotFoundCRLFException : public std::exception{
+    public:
+        const char *what(void) const throw();
+    };
+
+    class LineTooLongException : public std::exception{
+    public:
+        const char *what(void) const throw();
+    };
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -13,3 +13,57 @@ Client::Client(int client_fd, int client_port, std::string client_ip)
 Client::~Client() {
     close(_client_fd);
 }
+
+// read from client socket
+Client &Client::operator<<(const int socket_fd){
+    char buffer[BUFFER_SIZE] = {0, };
+    ssize_t recv_byte = recv(socket_fd, buffer, BUFFER_SIZE, 0);
+    if (recv_byte <= 0)
+        throw ClientDisconnectException();
+    _read_data += std::string(buffer, recv_byte);
+    return *this;
+}
+// write to client socket
+Client &Client::operator>>(const int socket_fd){
+    if (!_write_data.empty()){
+        ssize_t send_byte = send(socket_fd, _write_data.c_str(), _write_data.size(), 0);
+        if (send_byte <= 0)
+            throw ClientDisconnectException();
+        _write_data = _write_data.substr(send_byte);
+    }
+    return *this;
+}
+
+// data를 write data로 넣기
+Client &Client::operator<<(const std::string &line) {
+  _write_data += line;
+  return *this;
+}
+
+// 읽은 것을 string으로 내보내기
+Client &Client::operator>>(std::string &line) {
+    std::string::size_type pos = _read_data.find(CRLF);
+    if (pos == std::string::npos){
+        line.clear();
+        throw CannotFoundCRLFException();
+    }
+    if (pos + 2 > SOCKET_LINE_SIZE){
+        _read_data = _read_data.substr(pos + 2);    
+        throw LineTooLongException();
+    }
+    line = _read_data.substr(0, pos);
+    _read_data = _read_data.substr(pos + 2);
+    return *this;
+}
+
+const char* Client::ClientDisconnectException::what(void) const throw(){
+    return "Client disconnected";
+}
+
+const char* Client::CannotFoundCRLFException::what(void) const throw(){
+    return "Cannot found CRLF";
+}
+
+const char* Client::LineTooLongException::what(void) const throw(){
+    return "Command Line is Too long";
+}
