ef8a1fc7f45a605ab2c3ad828bc76e9817d9b21b
이지윤 <02leejiyun@naver.com>
2024-07-17 04:30:15 +0900

    feat: message parameter를 vector로 저장

--- includes/Message.hpp
+++ includes/Message.hpp
@@ -1,5 +1,5 @@
-#ifndef __MESSAGE_HPP__
-#define __MESSAGE_HPP__
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
 
 #include <iostream>
 #include <string>
@@ -8,40 +8,43 @@
 
 class Message {
 public:
-	typedef enum CMD {
-		NONE,  // 0
-		CAP,
-		PASS,
-		NICK,
-		USER,
-		QUIT,
-		JOIN,
-		PART,
-		TOPIC,
-		MODE,
-		INVITE,
-		KICK,
-		PRIVMSG,
-		PING,
-		CMD_SIZE
-	} e_cmd;
+    typedef enum CMD {
+        NONE,  // 0
+        CAP,
+        PASS,
+        NICK,
+        USER,
+        QUIT,
+        JOIN,
+        PART,
+        TOPIC,
+        MODE,
+        INVITE,
+        KICK,
+        PRIVMSG,
+        PING,
+        CMD_SIZE
+    } e_cmd;
 
 private:
-	static const std::string _commandList[CMD_SIZE + 1];
-	e_cmd _cmd_type;
-	std::string _params;
-	std::vector<std::string> splitN(const std::string& str, char delimiter, size_t n);
-	e_cmd findCommand(const std::string cmd_str);
+    static const std::string _commandList[CMD_SIZE + 1];
+    e_cmd _cmd_type;
+    std::string _cmd;
+    std::vector<std::string> _params;
+
+    e_cmd findCommand(const std::string &cmd);
+    std::vector<std::string> splitMessage(const std::string &str);
 
 public:
-	Message(void);
-	Message(std::string str);
-	~Message();
-	Message(const Message& obj);
-	Message& operator= (const Message& obj);
+    Message();
+    Message(std::string str);
+    ~Message();
+    Message(const Message& obj);
+    Message& operator= (const Message& obj);
 
-	e_cmd getCommand(void);
-	const std::string getParams(void);
+    e_cmd getCmdType();
+    const std::string getCmd();
+    const std::vector<std::string> getParams();
 };
 
 #endif
--- src/Message.cpp
+++ src/Message.cpp
@@ -1,60 +1,76 @@
 #include "../includes/Message.hpp"
 
 const std::string Message::_commandList[Message::CMD_SIZE + 1] = {
-	"NONE", "CAP", "PASS", 
-	"NICK", "USER", "QUIT", 
-	"JOIN", "PART", "TOPIC", 
-	"MODE", "INVITE", "KICK", 
-	"PRIVMSG", "PING"
+    "NONE", "CAP", "PASS", 
+    "NICK", "USER", "QUIT", 
+    "JOIN", "PART", "TOPIC", 
+    "MODE", "INVITE", "KICK", 
+    "PRIVMSG", "PING"
 };
 
+Message::Message() : _cmd_type(Message::NONE), _cmd(), _params() {}
 
-std::vector<std::string> Message::splitN(const std::string& str, char delimiter, size_t n) {
-    std::vector<std::string> tokens;
-    std::istringstream stream(str);
-    std::string token;
-    size_t count = 0;
-
-    while (std::getline(stream, token, delimiter) && count < n - 1) {
-        tokens.push_back(token);
-        count++;
+Message::Message(std::string str) : _cmd(), _params() {
+    std::vector<std::string> tokens = splitMessage(str);
+    if (!tokens.empty()) {
+        _cmd_type = findCommand(tokens[0]);
+        _cmd = tokens[0];
+        _params.assign(tokens.begin() + 1, tokens.end());
     }
-    // Add the remaining part of the string
-    if (stream) {
-        std::string remaining((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-        if (!token.empty())
-            remaining = token + delimiter + remaining;
-        tokens.push_back(remaining);
+}
+
+Message::~Message() {}
+
+std::vector<std::string> Message::splitMessage(const std::string &str) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream iss(str);
+
+    while (std::getline(iss, token, ' ')) {
+        // 끝에 \r 또는 \n이 오면 제거
+        std::size_t endPos = token.find_last_not_of("\r\n");
+        if (endPos != std::string::npos) {
+            token.erase(endPos + 1);
+        } else {
+            token.clear();
+        }
+        tokens.push_back(token.substr(0, endPos + 1));
     }
     return tokens;
 }
 
-Message::e_cmd Message::findCommand(const std::string cmd_str){
-	for(int i = 1; i <= Message::CMD_SIZE; i++){
-		if (cmd_str == _commandList[i]) return static_cast<e_cmd>(i);
-	}
-	return static_cast<e_cmd>(0);
+Message::e_cmd Message::findCommand(const std::string &cmd){
+    for(int i = 1; i <= Message::CMD_SIZE; i++) {
+        if (cmd == _commandList[i]) {
+            return static_cast<e_cmd>(i);
+        }
+    }
+    return static_cast<e_cmd>(0);
 }
 
-
-Message::Message(std::string str){
-	std::vector<std::string> ans = splitN(str, ' ', 2);
-	_cmd_type = findCommand(ans[0]);
-	_params = ans.size() == 1 ? "" : ans[1];
+Message::e_cmd Message::getCmdType() {
+    return _cmd_type;
 }
 
-Message::e_cmd Message::getCommand(void){return _cmd_type;}
-const std::string Message::getParams(void){return _params;}
-Message::Message(void) : _cmd_type(Message::NONE), _params("") {}
-Message::~Message(){}
-Message::Message(const Message& obj){
-	_cmd_type = obj._cmd_type;
-	_params = obj._params;
+const std::string Message::getCmd() {
+    return _cmd;
 }
-Message& Message::operator= (const Message& obj){
-	if (this != &obj){
-		_cmd_type = obj._cmd_type;
-		_params = obj._params;
-	}
-	return *this;
+
+const std::vector<std::string> Message::getParams() {
+    return _params;
+}
+
+Message::Message(const Message &obj) {
+    _cmd_type = obj._cmd_type;
+    _cmd = obj._cmd;
+    _params = obj._params;
+}
+
+Message& Message::operator=(const Message &obj) {
+    if (this != &obj){
+        _cmd_type = obj._cmd_type;
+        _cmd = obj._cmd;
+        _params = obj._params;
+    }
+    return *this;
 }
