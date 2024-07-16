f88ba23ccb410b5671881094d6dfa959140a395d
이지윤 <02leejiyun@naver.com>
2024-07-16 17:19:51 +0900

    feat: Client의 << 연산자 적용하여 NumericReply 전송

--- includes/NumericReply.hpp
+++ includes/NumericReply.hpp
@@ -5,11 +5,13 @@
 
 #define ERROR_REPLY(number, target, message) \
     (std::to_string(number) + " " + target + " " + message + "\r\n")
-#define NORMAL_REPLU(number, target, message) \
+#define NORMAL_REPLY(number, target, message) \
     (std::to_string(number) + " " + target + " " + message + "\r\n")
 
 #define ERR_NONICKNAMEGIVEN_431(target) ERROR_REPLY(431, target, ":No nickname given")
 #define ERR_ERRONEUSNICKNAME_432(target, invalidNick) ERROR_REPLY(432, target, invalidNick + " :Erroneus nickname")
 #define ERR_NICKNAMEINUSE_433(target, invalidNick) ERROR_REPLY(433, target, invalidNick + " :Nickname is already in use")
+
+#define RPL_NICKNAMECHANGE(target, oldNick) (":" + oldNick + " NICK " + target)
 
 #endif
--- src/nick.cpp
+++ src/nick.cpp
@@ -7,41 +7,37 @@ void Server::nick(Client *client, const std::string &nickname) {
 
     if (nickname.empty()) {
         // 431 ERR_NONICKNAMEGIVEN ":No nickname given"
-        std::string response = ERR_NONICKNAMEGIVEN_431(client->getNickname());
-        send(client->getSocket(), response.c_str(), response.size(), 0);
+        *client << ERR_NONICKNAMEGIVEN_431(client->getNickname());
         return;
     }
 
     if (!isValidNickname(nickname)) {
         // 432 ERR_ERRONEUSNICKNAME "<nick> :Erroneus nickname"
-        std::string response = ERR_ERRONEUSNICKNAME_432(client->getNickname(), nickname);
-        send(client->getSocket(), response.c_str(), response.size(), 0);
+        *client << ERR_ERRONEUSNICKNAME_432(client->getNickname(), nickname);
         return;
     }
 
     for (std::map<int, Client*>::iterator it = _clients.begin(); it != _clients.end(); ++it) {
         if (it->second->getNickname() == nickname) {
             // 433 ERR_NICKNAMEINUSE "<nick> :Nickname is already in use"
-            std::string response = ERR_NICKNAMEINUSE_433(client->getNickname(), nickname);
-            send(client->getSocket(), response.c_str(), response.size(), 0);
+            *client << ERR_NICKNAMEINUSE_433(client->getNickname(), nickname);
             return;
         }
     }
 
+    // TODO: 기존에 register되어 있었던 client인 경우
     std::string old_nickname = client->getNickname();
     client->setNickname(nickname);
-    // std::string response = ":" + old_nickname + " NICK " + client->getNickname() + "\r\n";
+    *client << RPL_NICKNAMECHANGE(client->getNickname(), old_nickname);
 }
 
 bool isValidNickname(const std::string &nickname) {
     if (nickname.length() > 9) {
         return false;
     }
-
     if ('0' <= nickname[0] && nickname[0] <= '9') {
         return false;
     }
-
     for (std::string::const_iterator it = nickname.begin(); it != nickname.end(); ++it) {
         if (!(isalnum(*it) || '_')) {
             return false;
