83ebaddd3a34e24936b98cc46d59ec063cd4ff0d
falconlee236 <falconlee236@cau.ac.kr>
2024-07-18 16:32:01 +0900

    bug fix

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -8,11 +8,12 @@ Channel::~Channel(){}
 Channel::Channel(void) : _name(""){
 	throw std::runtime_error("Channel(): consturctor is not allowed");
 }
-Channel::Channel(const Channel &obj){
+Channel::Channel(const Channel &obj) : _name(obj._name){
 	throw std::runtime_error("Channel(): copy consturctor is not allowed");
 }
 
 Channel& Channel::operator=(const Channel &obj){
+	(void) obj;
 	throw std::runtime_error("Channel(): operator= is not allowed");
 	return *this;
 }
