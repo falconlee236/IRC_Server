c3e5771f8cfec7dc421683882dbc2d6dc31dba2b
falconlee236 <falconlee236@cau.ac.kr>
2024-07-19 21:47:47 +0900

    remove first todo and leave second todo in quit.cpp function

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -1,10 +1,6 @@
 #include "../../includes/Server.hpp"
 
 void Server::quit(Client *client, const std::vector<std::string> params){
-    /*
-    //TODO - 지금은 welcome message가 되어야지 (즉 모든 정보가 입력되어야)
-    quit가 가능 그거 테스트해야함
-    */
     if (!client->isRegistered)
         *client << ERR_NOTREGISTERED_451(client->getNickname());
     else {
