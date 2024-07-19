c67b896a22291d22bd6781192a80e88d6a794e81
falconlee236 <falconlee236@cau.ac.kr>
2024-07-19 21:44:14 +0900

    add CLIENT_SOURCE macro in numeric reply

--- includes/NumericReply.hpp
+++ includes/NumericReply.hpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <iomanip>
 
+#define CLIENT_SOURCE(client) ((client).getNickname() + "!" + (client).getUsername() + "@" + (client).getHostname())
+
 #define ERROR_REPLY(number, target, message) \
     (std::to_string(number) + " " + target + " " + message + "\r\n")
 #define NORMAL_REPLY(number, target, message) \
@@ -12,8 +14,7 @@
 
 // NOTE - REGISTER
 #define RPL_WELCOME_001(client) \
-    NORMAL_REPLY(001, (client).getNickname(), ":Welcome to the Internet Relay Network " \
-        + (client).getNickname() + "!" + (client).getUsername() + "@" + (client).getHostname())
+    NORMAL_REPLY(001, (client).getNickname(), ":Welcome to the Internet Relay Network " + CLIENT_SOURCE((client)))
 #define RPL_YOURHOST_002(client) \
     NORMAL_REPLY(002, (client).getNickname(), ":Your host is " + (client).getServername() + " running version ircserv1.0")
 #define RPL_CREATED_003(client, date) \
@@ -33,10 +34,7 @@
 #define RPL_NICKNAMECHANGE(target, oldNick) (":" + oldNick + " NICK " + target + "\r\n")
 
 //NOTE - MODE_REPLIES
-//TODO - reply 가 좀 많이 부족한것 같은데, 이것 변경해야함.
-#define ERR_BADCHANMASK_476(client, channel)              \
-    ("476 " + (client).get_nickname() + " " + (channel) + \
-     " :Bad Channel Mask\r\n")
+#define ERR_BADCHANMASK_476(target, channel) ERROR_REPLY(476, target, (channel) + " :Bad Channel Mask")
 #define ERR_UMODEUNKNOWNFLAG_501(target) ERROR_REPLY(501, target, ":Unknown mode flag")
 
 #define RPL_BRDCAST_MODE(client, channel, mode, params)                      \
