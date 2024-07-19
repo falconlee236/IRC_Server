180f1ae480196081c9e8438434cc004e5bf3ab8c
falconlee236 <falconlee236@cau.ac.kr>
2024-07-19 14:16:02 +0900

    revise isRegistered function to variable

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -5,7 +5,7 @@ void Server::quit(Client *client, const std::vector<std::string> params){
     //TODO - 지금은 welcome message가 되어야지 (즉 모든 정보가 입력되어야)
     quit가 가능 그거 테스트해야함
     */
-    if (!client->isRegistered())
+    if (!client->isRegistered)
         *client << ERR_NOTREGISTERED_451(client->getNickname());
     else {
         //NOTE - default = client name
@@ -13,6 +13,6 @@ void Server::quit(Client *client, const std::vector<std::string> params){
         //TODO - channel이 있다면 channel도 삭제해야함.
         *client << msg + "\r\n";
         *client >> client->getSocket();
-        Server::removeClient(client->getSocket());
+        removeClient(client->getSocket());
     }
 }
