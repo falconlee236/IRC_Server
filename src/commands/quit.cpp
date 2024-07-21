169f5a978980ab4adc883c75c151693f4b656769
falconlee236 <falconlee236@cau.ac.kr>
2024-07-21 20:42:48 +0900

    add remove channel in quit.cpp

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -4,11 +4,18 @@ void Server::quit(Client *client, const std::vector<std::string> params){
     if (!client->isRegistered)
         *client << ERR_NOTREGISTERED_451(client->getNickname());
     else {
-        //NOTE - default = client name
         std::string msg = params.empty() ? client->getNickname() : params[0];
-        //TODO - channel이 있다면 channel도 삭제해야함.
+        for (std::set<Channel *>::iterator it = _channels.begin(); it != _channels.end(); ++it){
+            if ((*it)->isClientInChannel(client)){
+                (*it)->removeClient(client);
+                // client가 없으면 channel 삭제
+                if ((*it)->getClientNumber() == 0){
+                    delete (*it);
+                }
+            }
+        }
         *client << msg + "\r\n";
         *client >> client->getSocket();
-        removeClient(client->getSocket());
+        Server::removeClient(client->getSocket());
     }
 }
